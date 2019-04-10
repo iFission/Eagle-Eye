a446ea99c020df791602808ca57c6de6aa0d8765
iFission <ALEX@ALEX-MBA.local>
2019-04-10 19:13:31 +0800

    added functions to batch count, return dict

--- counter/counter.py
+++ counter/counter.py
@@ -1,33 +1,16 @@
 #%%
+from PIL import Image as PImage
+from IPython.display import display
+
+
+def dis_im(img):
+    display(PImage.fromarray(img))
+
+
+#%%
 import numpy as np
 import cv2
 print(cv2.__version__)
-
-#%%
-from matplotlib import pyplot as plt
-
-
-def dis_im(img):  # display opencv image using plt
-    plt.figure(figsize=(20, 20))  # show multiple images inline in jupyter
-    plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
-    # to convert cv's BGR to RGB, add [...,::-1]
-    plt.imshow(img, cmap='gray')  # display image using plot
-
-
-#%%
-class Person:
-    def __init__(self, id, x, y):
-        self.id = id
-        self.x = x
-        self.y = y
-        self.counted = False
-
-    def updateCoords(self, newX, newY):
-        self.x = newX
-        self.y = newY
-
-
-#%%
 import imutils
 from imutils.object_detection import non_max_suppression
 from imutils import paths
@@ -36,36 +19,87 @@ from imutils import paths
 # set Support Vector Machine to be pre-trained detector
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+
 #%%
-background = cv2.imread('counter/photo1/background.jpg',
-                        0)  # read image file in grayscale, to np array
-# dis_im(background)
-frame1 = cv2.imread('counter/photo1/2019-04-05_1232.jpg', 0)
-# dis_im(frame1)
-frame = cv2.imread('/Volumes/Media/Dls/persons/person_266.bmp')
-frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-frame = imutils.resize(frame, width=min(400, frame.shape[1]))
-# dis_im(frame)
+def count_people(path):
+    frame = cv2.imread(path)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    # frame = imutils.resize(frame, width=min(400, frame.shape[1]))
+    # dis_im(frame)
 
-# non-max supression combines multiple, overlapping bounding boxes to a single bounding box
+    # detect people in the image
+    (rects, weights) = hog.detectMultiScale(
+        frame, winStride=(4, 4), padding=(8, 8), scale=1.05)
 
-# detect people in the image
-(rects, weights) = hog.detectMultiScale(
-    frame, winStride=(4, 4), padding=(8, 8), scale=1.05)
+    # draw the original bounding boxes
+    for (x, y, w, h) in rects:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-# draw the original bounding boxes
-for (x, y, w, h) in rects:
-    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    # non-max supression combines multiple, overlapping bounding boxes to a single bounding box
+    # apply non-maxima suppression to the bounding boxes using a
+    # fairly large overlap threshold to try to maintain overlapping
+    # boxes that are still peoplem
+    rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+    pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
 
-# apply non-maxima suppression to the bounding boxes using a
-# fairly large overlap threshold to try to maintain overlapping
-# boxes that are still people
-rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
-pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+    # draw the final bounding boxes
+    for (xA, yA, xB, yB) in pick:
+        cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
 
-# draw the final bounding boxes
-for (xA, yA, xB, yB) in pick:
-    cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
+    # overlay text
+    cv2.putText(frame, "Count: " + str(len(pick)), (10, 20),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+    dis_im(frame)
+    return len(pick)
 
-dis_im(frame)
-print(len(pick))
+
+#%%
+import os
+
+
+# takes in path to an image folder, parse full image path, output as list
+def get_images_path(path):
+    images_path = []
+
+    files = os.listdir(f'{path}')
+    files.sort()
+
+    for file in files:
+        images_path.append(f'{path}/{file}')
+
+    return files, images_path
+
+
+#%%
+from tqdm import tqdm
+
+
+# count the number of people, from list of image paths
+def count_people_list(images_path):
+    count_list = []
+    for image_path in tqdm(images_path):
+        count_list.append(count_people(image_path))
+    return count_list
+
+
+#%%
+# count the number of people, from path provided
+def count_people_path(path, start=0, end=-1):
+    files, images_path = get_images_path(path)
+    count_list = count_people_list(images_path[start:end])
+
+    count_dict = {}
+    for file, count in zip(files, count_list):
+        # print(file, count)
+        count_dict[f"{file.rstrip('.jpg')}"] = count
+
+    return count_dict
+
+
+#%%
+from pprint import pprint
+# %time print(count_people_path('/Volumes/Media/Docs/Cloud/Drive/Eagle-Eye/counter/images/webcam_photos', 2000,2010)) # jupyter magic function, times the execution
+pprint(
+    count_people_path(
+        '/Volumes/Media/Docs/Cloud/Drive/Eagle-Eye/counter/images/photo2'))
