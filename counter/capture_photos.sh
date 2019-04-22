350aa5ccc2f39c2215226e39280cc2c2e16e3efc
iFission <ALEX@ALEX-MBA.local>
2019-04-22 16:36:00 +0800

    script to capture photo and rsync to server. runs on RPi

--- counter/capture_photos.sh
+++ counter/capture_photos.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-DATE=$(date +"%Y-%m-%d-%H$%M")
+DATE=$(date +%s)
+let DATE=$DATE/60*60
 
 fswebcam -r 1280x720 --no-banner ~/photos/$DATE.jpg
 
-rsync --archive ~/photos/ ubuntu@10.1.5.72:~/Eagle-Eye/counter/images/photos/
+rsync --archive -e "ssh -i ~/.ssh/ALEX-HERMES" ~/photos/ ubuntu@10.1.5.72:~/Eagle-Eye/counter/images/photos/
