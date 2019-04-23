4335effadda6385c344dade46731761a0f0a58ab
heyuhang0 <heyuhang0@outlook.com>
2019-04-23 14:24:47 +0800

    finalize predictor

--- predictor.py
+++ predictor.py
@@ -2,6 +2,8 @@ import time
 import numpy as np
 import backend
 from datetime import datetime, timedelta, timezone
+from sklearn import linear_model
+from sklearn.preprocessing import PolynomialFeatures
 
 
 def get_weekday_minutes(timestamp=-1):
@@ -17,11 +19,7 @@ def get_weekday_minutes(timestamp=-1):
     return weekday, minutes
 
 
-WEEKDAYS = [
-    'Monday', 'Tuesday', 'Wednesday', 'Thuresday', 'Friday', 'Saturday',
-    'Sunday'
-]
-
+WEEKDAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thuresday', 'Friday', 'Saturday', 'Sunday']
 
 def convert_input(data):
     train_set = {}
@@ -44,112 +42,52 @@ def convert_input(data):
     return train_set
 
 
-def get_future_time_points():
-    weekday, minutes = get_weekday_minutes()
-    return WEEKDAYS[weekday], np.array(
-        [[m * 60] for m in range(minutes, 60 * 24, 10)])
-
-
-def upload(x, y, node):
-    content = {}
-    for i in range(x.shape[0]):
-        content[f'{x[i][0]}-{x[i][1]}'] = int(y[i][0])
-    node.val = content
-
+def get_predict_set():
+    predict_set = {}
+    for day in WEEKDAYS:
+        predict_set[day] = np.arange(0, 60*60*24, 60*60).reshape(-1, 1)
+    return predict_set
 
 models = {}
+order = 3
 
 for d in WEEKDAYS:
     models[d] = linear_model.LinearRegression()
 
-
 def train(trainset):
     for day, (x, y) in trainset.items():
-        order = 2
         poly = PolynomialFeatures(order, include_bias=False)
         x = poly.fit_transform(x)
         models[day].fit(x, y)
 
-
-# can just save the model,can just call the linear regression
-
-
 def predict(day, x):
-    order = 2
     poly = PolynomialFeatures(order, include_bias=False)
     x = poly.fit_transform(x)
 
     return models[day].predict(x)
 
-
 def main():
-    room = 'Debug_Alex'  #backend.get_room_arg()
+    room = backend.get_room_arg()
     input_node = backend.FireBaseNode('NumberOfPeople', room, mode='r')
+    output_node = backend.FireBaseNode('Prediction', room)
 
     # convert data to numpy
     trainset = convert_input(input_node.val)
-    predict_day, predict_time = get_future_time_points()
+    predictset = get_predict_set()
 
     # Train model
+    print('Traning...')
     train(trainset)
 
     # Predict future time points
-    prediction = predict(predict_day, predict_time)
+    print('Predicting....')
+    result = {}
+    for d, x_array in predictset.items():
+        y_array = predict(d, x_array)
+        result[d] = {int(x_array[i]/3600): int(y_array[i]) for i in range(y_array.shape[0])}
+    print('Uploading....')
+    output_node.val = result
 
 
 if __name__ == "__main__":
     main()
-
-
-def convert_to_seconds(A):
-    # take one string as a input like '8:30'
-    y = A.split(':')
-    # print(y)
-    seconds = int(y[0]) * 3600 + int(y[1]) * 60 + int(y[2])
-    # print(seconds)
-    return seconds
-
-
-#convert_to_seconds('8:30:23')
-def convert_to_time(B):
-    hour = B // 3600
-    minutes = (B - 3600 * hour) // 60
-    seconds = B - 3600 * hour - 60 * minutes
-    return str(hour) + ':' + str(minutes) + ':' + str(seconds)
-
-
-#convert_to_time(30623)
-
-
-def time_table(C):
-    Second = convert_to_seconds(C)
-    lst = list(range(Second, 86430, 900))
-    return lst
-
-
-# time_table('8:30:30')
-
-
-def convert_time_table(lst):
-    result = []
-    index = 0
-    while index < len(lst):
-        #print(lst[index])
-        y = convert_to_time(lst[index])
-        result.append(y)
-        index += 1
-        #print(lst[index])
-    return result
-
-
-# z = time_table('8:30:30')
-# convert_time_table(z)
-
-import os
-import csv
-import numpy as np
-import numpy as np
-from sklearn import linear_model
-from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import PolynomialFeatures
