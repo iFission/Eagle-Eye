114daaafe653071d220f6a55d922ff267761a55e
iFission <ALEX@ALEX-MBA.local>
2019-04-23 16:50:52 +0800

    code clean up

--- counter/counter_old_files.py
+++ counter/counter_old_files.py
@@ -1,72 +1,65 @@
-#%%
 from PIL import Image as PImage
 from IPython.display import display
 from pprint import pprint
 import os
 import time
 from tqdm import tqdm
-
-#%%
-
 import numpy as np
+import time
 import cv2
-from imutils.object_detection import non_max_suppression
 print(cv2.__version__)
 
+import backend
+
+# define current working directory path
 cwd = os.getcwd()
 print(cwd)
 
+# define the path of incoming photos to be analysed
+path = f'{cwd}/counter/images/photos'
+
+# load HAAR cascade classifier
 headshoulder_cascade = cv2.CascadeClassifier(f'{cwd}/counter/reference/HS.xml')
 
+# define global variables
+files = []
+images_path = []
+background = []
+count_dict = {}
 
-def count_people(background, frame):
 
-    # dis_im(background)
-    # dis_im(frame)
+def count_people(background, frame, index):
+
     diff = cv2.absdiff(background, frame)
-    # dis_im(diff)
-    # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    # frame = imutils.resize(frame, width=min(400, frame.shape[1]))
-    # dis_im(frame)
-
-    # detect people in the image
+    # detect people in the image using the classifier with 2 sets of parameters
     headshoulder_rects = headshoulder_cascade.detectMultiScale(
         diff, scaleFactor=1.05, minNeighbors=4, flags=cv2.CASCADE_SCALE_IMAGE)
     headshoulder_rects_2 = headshoulder_cascade.detectMultiScale(
         diff, scaleFactor=1.02, minNeighbors=6, flags=cv2.CASCADE_SCALE_IMAGE)
 
-    # draw the original bounding boxes
+    # draw the bounding boxes
     for (x, y, w, h) in headshoulder_rects:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
     for (x, y, w, h) in headshoulder_rects_2:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-
-    # non-max supression combines multiple, overlapping bounding boxes to a single bounding box
-    # apply non-maxima suppression to the bounding boxes using a
-    # fairly large overlap threshold to try to maintain overlapping
-    # boxes that are still people
-    # rects1 = np.array([[x, y, x + w, y + h] for (x, y, w, h) in headshoulder_rects])
-    # rects2 = np.array([[x, y, x + w, y + h] for (x, y, w, h) in upperbody_rects])
-    # rects = np.vstack((rects1, rects2))
-    # pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
-    # pick = headshoulder_rects
-
-    # # draw the final bounding boxes
-    # for (xA, yA, xB, yB) in pick:
-    #     cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
 
     # overlay text, Count: #number
     cv2.putText(frame, "Count: " + str(len(headshoulder_rects)), (10, 50),
                 cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 0), 3, cv2.LINE_AA)
     cv2.putText(frame, "Count: " + str(len(headshoulder_rects_2)), (10, 110),
                 cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 0), 3, cv2.LINE_AA)
-    # dis_im(diff)
-    # dis_im(frame)
-    # save_im(frame, path)
+
+    save_im(
+        frame, f'{images_path[index]}.jpg'.replace('/photos/',
+                                                   '/photos_old_cv/').replace(
+                                                       'jpg', 'cv.jpg'))
+
+    # output the average count
     return (len(headshoulder_rects) + len(headshoulder_rects_2)) / 2
 
 
+# code to display image in jupyter notebook during debugging
 def dis_im(img):
     try:
         display(PImage.fromarray(img))
@@ -74,9 +67,9 @@ def dis_im(img):
         pass
 
 
-#%%
+# code to save image to folder
 def save_im(img, path):
-    cv2.imwrite(f"{path.replace('jpg', 'cv.jpg')}", img)
+    cv2.imwrite(f"{path}", img)
 
 
 # takes in path to an image folder, parse full image path, output as list
@@ -86,7 +79,7 @@ def get_images_path(path):
     files = os.listdir(f'{path}')
     files.sort()
 
-    files = [file.rstrip('.jpg') for file in files if file.endswith('.jpg')]
+    files = [file.rstrip('.jpg') for file in files if file.endswith('0.jpg')]
     images_path = [f'{path}/{file}' for file in files]
     return files, images_path
 
@@ -106,79 +99,42 @@ def update_background(index):
             return False
     index = 0 if index - 1 < 0 else index - 1
     background = cv2.imread(f'{images_path[index]}.jpg')
-    dis_im(backend)
-    print(f'updated background to index {index}')
+    save_im(
+        background,
+        f'{images_path[index]}_background.jpg'.replace('/photos/',
+                                                       '/photos_old_cv/'))
+    print(f'updated background to {files[index]}')
     return True
-
-
-#%%
-# get_images_path(
-#     '/Volumes/Media/Docs/Cloud/Drive/Eagle-Eye/counter/images/photo1')
-# print(files, images_path)
-
-#%%
-files = []
-images_path = []
-background = []
-count_dict = {}
-#%%
-# pprint(count_dict)
-# dis_im(background)
-# update_background()
-# dis_im(background)
 
 
 def count_people_now(timestamp, path):
     get_images_path(path)
-    # print(files)
+
     counted = False
     timestamp = str(timestamp)
+
+    # keep looping until the timestamp file appears and is counted
     while not counted:
         if str(timestamp) in files:
-            index = files.index(timestamp)
-            update_background(index)
-            frame = cv2.imread(f'{images_path[index]}.jpg')
-            # count = count_people(background, frame)
-            count = count_people(background, frame)
-            # print(count)
-            # count = np.random.randint(0, 2)
-            append_dict(timestamp, count)
+            index = files.index(
+                timestamp)  # get the index of the current timestamp
+            update_background(index)  # update background if necessary
+            frame = cv2.imread(
+                f'{images_path[index]}.jpg')  # load frame to opencv object
+            count = count_people(background, frame,
+                                 index)  # call count people cv
+            append_dict(timestamp,
+                        count)  # append the count to local dictionary
             counted = True
             return count
         else:
             time.sleep(1)
-
-
-# print(
-#     count_people_now(
-#         '/Volumes/Media/Docs/Cloud/Drive/Eagle-Eye/counter/images/photo1',
-#         1554438540))
-# print(count_dict)
-
-#%%
-# %time print(count_people_path('/Volumes/Media/Docs/Cloud/Drive/Eagle-Eye/counter/images/webcam_photos', 2000,2010)) # jupyter magic function, times the execution
-# dict = count_people_path(
-#     '/Volumes/Media/Docs/Cloud/Drive/Eagle-Eye/counter/images/photo1', 0, 3)
-
-#%%
-# print(dict)
-
-#%%
-import time
-
-# print(int(time.time() // 60 * 60))
-# print(time.asctime(time.localtime(time.time())))
-#%%
-import backend
-
-path = f'{cwd}/counter/images/photos'
+            get_images_path(path)
 
 
 def main():
     node = backend.FireBaseNode('NumberOfPeople', mode='a')
     while True:
-        # Write your code here
-        # timestamp = int(time.time() // 60 * 60)
         t_ls, i_ls = get_images_path(path)
         print(t_ls, i_ls)
 
@@ -188,14 +144,13 @@ def main():
             except Exception as e:
                 print(e)
                 print(timestamp)
-                input("Press anykey to continue")
+                # input("Press anykey to continue")
                 num_people = 0
-            # num_people = np.random.randint(0, 2)
 
-            # Upload to firebase
+            # upload to firebase
             node.append(num_people, timestamp=timestamp)
             localtime = time.asctime(time.localtime(time.time()))
-            # print(f'{localtime}: NumberOfPeople={num_people}')
+            print(f'{localtime}: NumberOfPeople={num_people}')
 
 
 if __name__ == "__main__":
